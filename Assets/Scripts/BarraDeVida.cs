3d78c2c54cfcdc5cad7ae3f3c681acc0a88ef30c
Zac118 <augustozanettilima@gmail.com>
2026-05-30 18:42:06 -0300

    Fiz a barra de vida
    A Tai vai botar depois na Main

--- Assets/Scripts/BarraDeVida.cs
+++ Assets/Scripts/BarraDeVida.cs
@@ -1,6 +1,13 @@
 using UnityEngine;
+using UnityEngine.UI; // necessário para Slider/Image
 
 public class BarraDeVida : MonoBehaviour
 {
+    public PlayerController playerController; // arraste o Player aqui no Inspector
+    public Slider sliderVida;                 // arraste o Slider da UI aqui
 
+    void Update()   
+    {
+        sliderVida.value = playerController.dinheiroAtual;
+    }
 }
